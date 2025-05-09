Fix: Adjust remedycount scaling for proper calculations

config_variables.py
@@ -25,7 +25,8 @@ remediocount_exp = [np.exp(-i * 6* 0.001)**-1 for i in range(1001)] # Array ofex
 max_exp = max(remediocount_exp)
 remediocount_exp = [val / max_exp for val in remediocount_exp]  # Normalizing to start at 1 and end at 0
 
-num_levels = len(remediocount_linear)                       # Size of the vector that stores the antiviral values
+remediocount = remediocount_linear
+num_levels = len(remediocount)                       # Size of the vector that stores the antiviral values
 
 # Parameters for the Runge-Kutta method
 h = 0.01                                             # Precision parameter for the Runge-Kutta numerical method